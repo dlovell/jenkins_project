347f29d6da92b7300eb60f47b7179fe320b5557f
dlovell <dlovell@gmail.com>
2014-02-20 22:01:10 -0500

    initial commit

---
+++ setup_jenkins.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+set -e
+set -v
+
+
+# settings
+jenkins_plugins=(
+	scm-api
+	credentials
+	ssh-credentials
+	git-client
+	git
+	github-api
+	github
+	jenkins-flowdock-plugin
+	)
+jenkinsapi_version=0.1.13
+jar_dir=/var/cache/jenkins/war/WEB-INF/
+jenkins_uri=http://127.0.0.1:8080/
+
+
+# Helper functions
+function install_jenkins () {
+	# per http://pkg.jenkins-ci.org/debian-stable/
+	wget -q -O - http://pkg.jenkins-ci.org/debian-stable/jenkins-ci.org.key | sudo apt-key add -
+	sudo echo "deb http://pkg.jenkins-ci.org/debian-stable binary/" >> /etc/apt/sources.list
+	sudo apt-get update
+	sudo apt-get install -y jenkins
+}
+
+function install_jenkins_plugin () {
+	plugin_name=$1
+	plugin_dir=${jar_dir}/plugins/
+	#
+	rm ${plugin_dir}/${plugin_name}.hpi 2>/dev/null || true
+	java -jar ${jar_dir}/jenkins-cli.jar -s $jenkins_uri install-plugin $plugin_name
+	return 0
+}
+
+function restart_jenkins () {
+	java -jar ${jar_dir}/jenkins-cli.jar -s $jenkins_uri safe-restart || true
+	return 0
+}
+
+function wait_for_web_response () {
+	set +e
+	uri=$1
+	wget $uri 2>/dev/null 1>/dev/null
+	while [ $? -ne "0" ]
+	do
+		sleep 2
+		wget $uri 2>/dev/null 1>/dev/null
+	done
+	set -e
+	return 0
+}
+
+
+install_jenkins
+# make sure jenkins api available for job setup automation
+pip install jenkinsapi==$jenkinsapi_version
+
+
+# wait for jenkins to respond before installing
+wait_for_web_response $jenkins_uri
+# install plugins
+for jenkins_plugin in ${jenkins_plugins[*]}; do
+	install_jenkins_plugin $jenkins_plugins
+done
+# restart before continuing
+restart_jenkins
+# wait for jenkins to respond before returning
+wait_for_web_response $jenkins_uri
+
+
+# miscellaneous support operations
+#
+# set up headless matplotlib
+jenkins_home=/var/lib/jenkins/
+mkdir -p ${jenkins_home}/.matplotlib
+echo backend: Agg > ${jenkins_home}/.matplotlib/matplotlibrc
+chown -R jenkins $jenkins_home
+#
+# make sure jenkins can install python packages
+python_dir=/usr/local/lib/python2.7/dist-packages
+chown -R jenkins $python_dir
---
+++ spin_up_jenkins_server.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+set -e
+set -v
+
+
+# modifiable setings
+project_name=$1
+if [ -z $project_name ]; then
+	echo "project_name must not be blank"
+	exit 1
+fi
+
+
+# get some path info
+this_script_abs_path=$(readlink -f "$0")
+this_script_dirname=$(dirname $this_script_abs_path)
+# everything is relative to this script's dir
+cd $this_script_dirname
+
+
+settings_script=settings.sh
+jenkins_setup_script=setup_jenkins.sh
+jenkins_utils_script=jenkins_utils.py
+open_port_script=open_master_port_via_starcluster_shell.py
+config_filename_suffix=.config.xml
+#
+jenkins_project_name=jenkins_project
+probcomp_base_uri=https://github.com/mit-probabilistic-computing-project
+jenkins_repo_uri=$probcomp_base_uri/$jenkins_project_name
+
+
+# set some defaults
+project_dir=$project_name
+cluster_name=$project_name
+cluster_template=$project_name
+instance_type=c1.xlarge
+num_intances=1
+# pull from project settings.sh if it exists
+settings_script=$project_dir/$settings_script
+if [ -f $settings_script]; then
+	. $settings_script
+fi
+
+
+# spin up the cluster
+starcluster start -c $cluster_template -i $instance_type -s $num_intances $cluster_name
+hostname=$(starcluster listclusters $cluster_name | grep master | awk '{print $NF}')
+# bypass key checking
+ssh -o PasswordAuthentication=no -o StrictHostKeyChecking=no jenkins@$hostname exit || true
+
+
+# open up the port for jenkins
+starcluster shell < <(perl -pe "s/'crosscat'/'$cluster_name'/" $open_port_script)
+# set up jenkins
+starcluster sshmaster $cluster_name "(git clone $jenkins_repo_uri)"
+starcluster sshmaster $cluster_name bash $jenkins_project_name/$jenkins_setup_script
+
+
+# push up jenkins configuration
+# jenkins server must be up and ready
+jenkins_uri=http://$hostname:8080
+for config_filename in $project_dir/*$config_filename_suffix; do
+	job_name=$(basename ${config_filename%$config_filename_suffix})
+	python $jenkins_utils_script \
+	--base_url $jenkins_uri \
+	--config_filename $config_filename \
+	--job_name $job_name \
+	-create
+done
+
+
+# notify user what hostname is
+echo $hostname
