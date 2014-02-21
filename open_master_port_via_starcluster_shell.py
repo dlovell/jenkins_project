f2bb5d420ded24718db8bf05b66dd216d4b13532
dlovell <dlovell@gmail.com>
2014-02-21 13:57:03 -0500

    parse arguments for cluster_name

--- open_master_port_via_starcluster_shell.py
+++ open_master_port_via_starcluster_shell.py
@@ -1,38 +1,44 @@
-# USAGE: starcluster shell < open_master_port_via_starcluster.py
+# USAGE: starcluster shell < open_master_port_via_starcluster.py <CLUSTER_NAME>
+import argparse
+import functools
+import time as imported_time
 
-# settings
-port = 8080
-service_name = 'jenkins'
-cluster_name = 'crosscat'
-protocol = 'tcp'
-world_cidr = '0.0.0.0/0'
+
+# unfortunate kludge b/c of 'starcluster shell'
+parser = argparse.ArgumentParser()
+parser.add_argument('_shell', type=str)
+parser.add_argument('cluster_name', type=str)
+parser.add_argument('--service_name', default='jenkins', type=str)
+parser.add_argument('--port', default=8080, type=int)
+args = parser.parse_args()
+if 'args' not in locals():
+    exit()
+cluster_name = args.cluster_name
+service_name = args.service_name
+port = args.port
+
+
+# always the same
+port_min, port_max = port, port
+protocol='tcp'
+world_cidr='0.0.0.0/0'
+
 
 master = cm.get_cluster(cluster_name).master_node
 group = master.cluster_groups[0]
+get_port_open = functools.partial(master.ec2.has_permission, group, protocol,
+        port, port, world_cidr)
 
-if isinstance(port, tuple):
-    port_min, port_max = port
-    pass
-else:
-    port_min, port_max = port, port
-    pass
-
-def get_port_open():
-    # refresh master and group; not sure if necessary
-    master = cm.get_cluster(cluster_name).master_node
-    group = master.cluster_groups[0]
-    #
-    return master.ec2.has_permission(group, protocol, port_min, port_max, world_cidr)
 
 if not get_port_open():
-    import time as imported_time
-    log_str = "Authorizing tcp ports [%s-%s] on %s for: %s" % \
-            (port_min, port_max, world_cidr, service_name)
+    log_str = "Authorizing tcp port on %s for: %s" % \
+            (port, world_cidr, service_name)
     print log_str
     master.ec2.conn.authorize_security_group(
-            group_id=group.id, ip_protocol='tcp',
-            from_port=port_min,
-            to_port=port_max, cidr_ip=world_cidr)
-    while not get_port_open():
-        imported_time.sleep(2)
+            group_id=group.id, ip_protocol=protocol,
+            from_port=port, to_port=port,
+            cidr_ip=world_cidr)
 
+while not get_port_open():
+    # don't return till port is open
+    imported_time.sleep(2)
